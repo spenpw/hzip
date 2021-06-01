2a321fa59e1b709b5c473f5f524abb26fbab6056
Paul Spencer <spenpw@gmail.com>
2021-06-01 11:58:32 -0600

    Create tree data structure with operations

--- huffman_tree.c
+++ huffman_tree.c
@@ -1,1 +1,34 @@
+#include "huffman_tree.h"
+#include <stdlib.h>
+#include <assert.h>
 
+struct HTree* combine_trees(struct HTree* tree_1, struct HTree* tree_2) {
+    assert(tree_1 != NULL);
+    assert(tree_2 != NULL);
+    struct HTree* new_tree = calloc(1, sizeof(struct HTree));
+    if (tree_1->freq <= tree_2->freq) {
+        new_tree->left = tree_1;
+        new_tree->right = tree_2;
+    } else {
+        new_tree->left = tree_2;
+        new_tree->right = tree_1;
+    }
+    new_tree->freq = tree_1->freq + tree_2->freq;
+    return new_tree;
+}
+
+struct HTree* create_leaf_tree(int freq, char data) {
+    struct HTree* new_tree = calloc(1, sizeof(struct HTree));
+    new_tree->freq = freq;
+    new_tree->is_leaf = true;
+    new_tree->leaf_data = data;
+    return new_tree;
+}
+
+void destroy_tree(struct HTree* tree) {
+    if (!tree->is_leaf) {
+        destroy_tree(tree->left);
+        destroy_tree(tree->right);
+    }
+    free(tree);
+}
---
+++ huffman_tree.h
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+
+struct HTree {
+    struct HTree* left;
+    struct HTree* right;
+    int freq;
+    bool is_leaf;
+    char leaf_data;
+};
+
+struct HTree* combine_trees(struct HTree* tree_1, struct HTree* tree_2);
+struct HTree* create_leaf_tree(int freq, char data);
+void destroy_tree(struct HTree* tree);
