39269e752c58c31514f2513c3a85d1f63f7812cd
Paul Spencer <spenpw@gmail.com>
2021-06-02 13:28:41 -0600

    Add huffman coding algorithm test

--- htree_test.c
+++ htree_test.c
@@ -1,6 +1,36 @@
 #include <assert.h>
+#include "huffman_tree.h"
+#include "pqueue.h"
 
 int main(int argc, char** argv) {
-    assert("NOT IMPLEMENTED" == 0);
+
+    // Run huffman tree algorithm- this depends on a functioning pqueue
+
+    struct PQueue* queue = create_pqueue();
+
+    pqueue_insert(queue, create_leaf_tree(10, 'a'), 10);
+    pqueue_insert(queue, create_leaf_tree(10, 'b'), 9);
+    pqueue_insert(queue, create_leaf_tree(10, 'c'), 8);
+    pqueue_insert(queue, create_leaf_tree(10, 'd'), 7);
+    pqueue_insert(queue, create_leaf_tree(10, 'e'), 6);
+    pqueue_insert(queue, create_leaf_tree(10, 'f'), 5);
+    pqueue_insert(queue, create_leaf_tree(10, 'g'), 4);
+    pqueue_insert(queue, create_leaf_tree(10, 'h'), 3);
+    pqueue_insert(queue, create_leaf_tree(10, 'i'), 2);
+    pqueue_insert(queue, create_leaf_tree(10, 'j'), 1);
+
+    while (queue->size > 1) {
+        struct HTree* new_tree = combine_trees(pqueue_pop(queue), pqueue_pop(queue));
+        pqueue_insert(queue, new_tree, new_tree->freq);
+    }
+
+    struct HTree* tree = pqueue_pop(queue);
+
+    assert(!tree->is_leaf);
+    assert(!tree->left->is_leaf);
+    assert(tree->left->left->left->leaf_data == 'f');
+
+
+
     return 0;
 }
