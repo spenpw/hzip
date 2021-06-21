5f83ff48800ea0e48072ea134ac0b15d5da63fde
Paul Spencer <spenpw@gmail.com>
2021-06-21 20:27:50 +0000

    Useful assertions for debugging

--- huffman_tree.c
+++ huffman_tree.c
@@ -7,6 +7,7 @@ struct HTree* combine_trees(struct HTree* tree_1, struct HTree* tree_2) {
     assert(tree_1 != NULL);
     assert(tree_2 != NULL);
     struct HTree* new_tree = calloc(1, sizeof(struct HTree));
+    assert(new_tree);
     if (tree_1->freq <= tree_2->freq) {
         new_tree->left = tree_1;
         new_tree->right = tree_2;
@@ -21,6 +22,7 @@ struct HTree* combine_trees(struct HTree* tree_1, struct HTree* tree_2) {
 
 struct HTree* create_leaf_tree(unsigned int freq, char data) {
     struct HTree* new_tree = calloc(1, sizeof(struct HTree));
+    assert(new_tree);
     new_tree->freq = freq;
     new_tree->is_leaf = true;
     new_tree->leaf_data = data;
@@ -37,6 +39,10 @@ void destroy_tree(struct HTree* tree) {
 
 void insert_tree_to_table(struct HTree* tree, struct KeyTable table, unsigned int prefix_data, unsigned int prefix_length) {
     if (tree->is_leaf) {
+        // assert(!tree->left);
+        if (tree->left || tree->right) {
+            assert(0);
+        }
         set_key_table_value(table, tree->leaf_data, prefix_data, prefix_length);
     } else {
         unsigned int next_data = prefix_data << 1;
